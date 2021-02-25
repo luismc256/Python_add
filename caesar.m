99a7f51f2031de0d3487550a1717de98b63283b1
Luis Almeida <luismc256@yahoo.com>
2021-02-25 09:23:33 -0500

    agregue caesar.m

---
+++ caesar.m
@@ -0,0 +1,13 @@
+function coded=caesar(text, n)
+m=n-95*floor(n/95);
+a=text+m
+for i=1:length(a)
+       if a(i)>126
+           a(i)=32+(a(i)-127);
+       end
+       if a(i)<32
+           a(i)=126-(31-a(i));
+       end
+    
+end
+coded = char(a);
